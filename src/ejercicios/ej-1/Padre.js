5d02bddc95dd8ac052e053837ffd261158f5ebd1
martinho <martinho.cores@gmail.com>
2020-07-15 11:51:48 +0200

    ejercicios-react-1

--- src/ejercicios/ej-1/Padre.js
+++ src/ejercicios/ej-1/Padre.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import Hijo from "./Hijo";
+import { propTypes } from "react-bootstrap/esm/Image";
 
 const Padre = () => {
-    const text = "Hola Mundo";
-    return (
-        <div className="alert alert-primary">
-            <h4>Padre</h4>
-            <Hijo />
-        </div>
-    );
+  const text = "Hola Mundo";
+  return (
+    <div className="alert alert-primary">
+      <h4>Padre</h4>
+      <Hijo text={text} />
+    </div>
+  );
 };
 
 export default Padre;
